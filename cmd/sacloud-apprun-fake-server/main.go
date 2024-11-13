8f08a318f04bc16b031d504a735743922c57ba4d
hekki <contact@hekki.info>
2024-11-13 11:22:29 +0900

    fix typo

--- cmd/sacloud-apprun-fake-server/main.go
+++ cmd/sacloud-apprun-fake-server/main.go
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 The sacloud/object-storage-api-go authors
+// Copyright 2021-2024 The sacloud/apprun-api-go authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
